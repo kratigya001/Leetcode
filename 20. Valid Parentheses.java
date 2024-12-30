aa7262818ce6a028f56ec99814f31f5213ab6236
kratigya001 <kratigyabansal80@gmail.com>
2024-12-30 20:01:43 +0530

    easy

---
+++ 20. Valid Parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> st= new Stack<Character>();
+        for(char ch :s.toCharArray()){
+            if(st.empty() || ch =='(' || ch=='{' || ch=='['){
+                st.push(ch);
+                continue;
+            }
+            if(ch ==')'){
+                if(st.peek()=='('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(ch =='}'){
+                if(st.peek()=='{'){
+                    st.pop();
+                } else{
+                    return false;
+                }
+            }
+            else if(ch==']'){
+                if(st.peek()=='['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+        
+    }
+}
