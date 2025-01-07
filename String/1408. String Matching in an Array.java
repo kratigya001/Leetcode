1a01fe934b8de34bf259c35c0a970d78b09957df
kratigya001 <kratigyabansal80@gmail.com>
2025-01-07 14:25:40 +0530

    easy

---
+++ String/1408. String Matching in an Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        ArrayList<String> str=new ArrayList<>();
+        int n=words.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(words[j].contains(words[i])){
+                    str.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return str;
+    }
+}
