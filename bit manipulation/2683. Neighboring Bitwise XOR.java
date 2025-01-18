9bdb07bdafa17b325a61e556ca8b07abc9786166
kratigya001 <kratigyabansal80@gmail.com>
2025-01-18 20:00:43 +0530

    update

--- bit manipulation/2683. Neighboring Bitwise XOR.java
+++ bit manipulation/2683. Neighboring Bitwise XOR.java
@@ -7,3 +7,31 @@ class Solution {
         return x == 0;
     }
 }
+
+//another method 
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int n=derived.length;
+        int original[]=new int[n];
+
+        original[0]=0;
+        for(int i=0;i<n-1;i++){
+            original[i+1]=original[i]^derived[i];
+        }
+        if((original[n-1] ^ original[0]) == derived[n-1]){
+            return true;
+        }
+
+        original[0]=1;
+        for(int i=0;i<n-1;i++){
+            original[i+1]=original[i]^derived[i];
+        }
+         if((original[n-1]^original[0])== derived[n-1]){
+
+          return true;
+         }
+         return false;
+
+        
+    }
+}
